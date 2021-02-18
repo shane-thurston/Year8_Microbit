e1b17dedc40ed192235be392391f187480d1de57
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2021-02-18 12:41:33 +1100

    Update images_with_buttons.py

--- images_with_buttons.py
+++ images_with_buttons.py
@@ -2,5 +2,8 @@ from microbit import *
 while True:
   if button_a.is_pressed():
     display.show(Image.HAPPY)
+    sleep(500)
+    display.show(Image.MEH)
+    sleep(500)
   else:
     display.show(Image.SAD)
