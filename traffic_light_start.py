4cbd799b2a73cce43c365baefd548282ecb1f774
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2021-04-23 08:32:56 +1000

    Create traffic_light_start.py
    Starting code for the micro:bit traffic light task.

---
+++ traffic_light_start.py
@@ -0,0 +1,14 @@
+from microbit import *
+for count in range(10):
+    pin0.write_digital(1)
+    sleep(500)
+    pin0.write_digital(0)
+    sleep(500)
+    pin1.write_digital(1)
+    sleep(500)
+    pin1.write_digital(0)
+    sleep(500)
+    pin2.write_digital(1)
+    sleep(500)
+    pin2.write_digital(0)
+    sleep(500)
